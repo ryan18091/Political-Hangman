9942dea3bff66d0540116c7a74bcd4f863e873b9
Ryan Erickson <ryanerickson74@gmail.com>
2017-10-24 13:40:28 -0500

    id change

--- pol_ids.py
+++ pol_ids.py
@@ -4,4 +4,7 @@ t_dict = {'Donald Trump': 25073877, 'Bernie Sanders': 216776631,'Mike Pence': 22
           'Ryan Zinke': 1906282568, 'Sonny Perdue':842072478834909184, 'Wilbur L. Ross, Jr.': 836305915452272641,
           'Alexander Acosta': 819980276570976256, 'Benjamin S. Carson, Sr.':828613457020870657, 'Elaine L. Chao': 826065858548133888,
           'James Richard Perry': 830128820447539208, 'Elisabeth Prince DeVos': 828997388920619008,
-          'David J. Shulkin':831482588246196225, 'John F. Kelly':891094042422853633, 'Daniel Coats':212262370 }
+          'David J. Shulkin':831482588246196225, 'John F. Kelly':891048045361733636, 'Daniel Coats':212262370 }
+
+
+
