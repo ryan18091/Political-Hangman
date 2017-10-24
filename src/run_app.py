07bc1377a1edb39a3eae63dad49bcbd3be4aaf09
Ryan Erickson <ryanerickson74@gmail.com>
2017-10-24 17:33:34 -0500

    Adding required Heroku files.

---
+++ src/run_app.py
@@ -0,0 +1,32 @@
+import os
+import sqlite3
+
+from src.twitter_app import app
+
+# from twitter_apptest import app
+
+
+conn = sqlite3.connect('politicalhangman.db')
+c = conn.cursor()
+
+def create_table():
+    c.execute('CREATE TABLE IF NOT EXISTS game_db(session_id INTEGER, politician VARCHAR, politician_id INTEGER,'
+              'datestamp TEXT, guess_phrase TEXT, phrase TEXT, word_guess TEXT, turns INTEGER, alpl TEXT)')
+    conn.commit()
+    c.close()
+    conn.close()
+
+create_table()
+
+
+
+if __name__ == '__main__':
+    app.debug = False
+    host = os.environ.get('IP', '0.0.0.0')
+    # port = int(os.environ.get('PORT', 8080))
+    port = int(os.environ.get('PORT', 8910))
+
+    app.run(host=host, port=port)
+
+#Background image distorted on hangman game screen
+#replace &amp with &
