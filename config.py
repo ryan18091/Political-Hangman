397576b01ccaadc1e92c152bb5ca2610001360aa
Ryan Erickson <ryanerickson74@gmail.com>
2017-10-28 07:05:52 -0500

    updated working directory

--- config.py
+++ config.py
@@ -1,7 +1,16 @@
 import os
 
 
-#default config
+# default config
 class BaseConfig(object):
     DEBUG = False
-    
+    SQLALCHEMY_DATABASE_URI = os.environ['sqlite:///political_hangmanPSQL.db']
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+class DevelopmentConfig(BaseConfig):
+    DEBUG = True
+
+
+class ProductionConfig(BaseConfig):
+    DEBUG = False
