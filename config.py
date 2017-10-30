08e3e0dd3b16be7bde5a7e1c086eb854ec150c9f
Ryan Erickson <ryanerickson74@gmail.com>
2017-10-30 14:17:42 -0500

    changed secret key

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ import os
 # default config
 class BaseConfig(object):
     DEBUG = False
+    SECRET_KEY = '\xfd{H\xe5<\x95\xf9\xe3\x96.5\xd1\x01O<!\xd5\xa2\xa0\x9fR"\xa1\xa8'
     SQLALCHEMY_DATABASE_URI = os.environ['sqlite:///political_hangmanPSQL.db']
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
 class DevelopmentConfig(BaseConfig):
@@ -13,4 +13,4 @@ class DevelopmentConfig(BaseConfig):
 
 
 class ProductionConfig(BaseConfig):
-    DEBUG = False
+    DEBUG = Fals
