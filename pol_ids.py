56d8300fbee42ba60c4933b2a904b55ccf6306ba
Ryan Erickson <ryanerickson74@gmail.com>
2017-10-31 13:08:57 -0500

    changed secret key

--- pol_ids.py
+++ pol_ids.py
@@ -1,10 +1,11 @@
 t_dict = {'Donald Trump': 25073877, 'Bernie Sanders': 216776631,'Mike Pence': 22203756, 'Rex Tillerson':
-    807824558631763968, 'Richard Shelby': 21111098,'Luther Strange': 15141113,'Lisa Murkowski': 151247615,
-          'Dan Sullivan': 2891210047, 'Steven T. Mnuchin':799764016885329921, 'Jeff Sessions':986781648,
+    807824558631763968, 'Richard Shelby': 21111098,'Dan Sullivan': 2891210047, 'Steven T. Mnuchin':799764016885329921, 'Jeff Sessions':986781648,
           'Ryan Zinke': 1906282568, 'Sonny Perdue':842072478834909184, 'Wilbur L. Ross, Jr.': 836305915452272641,
           'Alexander Acosta': 819980276570976256, 'Benjamin S. Carson, Sr.':828613457020870657, 'Elaine L. Chao': 826065858548133888,
           'James Richard Perry': 830128820447539208, 'Elisabeth Prince DeVos': 828997388920619008,
-          'David J. Shulkin':831482588246196225, 'John F. Kelly':891048045361733636, 'Daniel Coats':212262370 }
+          'David J. Shulkin':831482588246196225, 'John F. Kelly':891048045361733636, 'Daniel Coats':212262370,
+          'Luther Strange':829794295355940866, 'Lisa Murkowski':18061669,'John McCain':  19394188, 'Jeff Flake':16056306,
+          'John Boozman':5558312, 'Bradley Byrne':2253968388 }
 
 
 
