0b3a45fb6994dfb4876b7181c8b58fe3c5cb43fb
Ryan Erickson <ryanerickson74@gmail.com>
2017-11-01 13:24:17 -0500

    changed the way engine relates to non-existing pre-tweets and moved tweet delete and entry to if statements

--- db_engine.py
+++ db_engine.py
@@ -60,15 +60,8 @@ while True:
     for politician, politician_id in t_dict.items():
 
         time.sleep(6)
-        # for tweet in tweepy.Cursor(api.user_timeline, id=politician_id).items(10):
         for tweet in tweepy.Cursor(api.user_timeline, id=politician_id, tweet_mode='extended').items(10):
 
-            # print(tweet)
-            # print(type(tweet))
-            #
-            import json
-            # print(dir(tweet))
-            # print(tweet.full_text)
 
             if (not tweet.retweeted) and ('RT @' not in tweet.full_text):
                 try:
@@ -76,26 +69,7 @@ while True:
                     i = (tweet.full_text)
                     created_at = (tweet.created_at)
                     mystring = str(t)
-                    # print(i)
-                    #                    print(json.loads(t)['full_text'])
 
-                    # print(mystring)
-                    #
-                    # import json
-                    #
-                    # json_input = '{ "one": 1, "two": { "list": [ {"item":"A"},{"item":"B"} ] } }'
-
-                    # try:
-                    #     decoded = json.loads(tweet)
-                    #
-                    #     # pretty printing of json-formatted string
-                    #     # print(json.dumps(decoded, sort_keys=True, indent=4))
-                    #
-                    #     print(decoded['id'])
-                    #     # print(decoded['two']['list'][1]['item'])
-                    #
-                    # except (ValueError, KeyError, TypeError):
-                    #     print("JSON format error")
 
 
                     def find_between1(first, last):
@@ -112,7 +86,6 @@ while True:
 
 
                     background = (find_between1('profile_banner_url' , 'profile_link_color'))
-                    # print(background)
 
                     def find_between2(first, last):
                         try:
@@ -127,54 +100,34 @@ while True:
 
 
                     profile = (find_between2('profile_image_url', 'profile_image_url_https'))
-                    # print(profile)
 
-                    # conn = sqlite3.connect('politicalhangman.db')
-                    # c = conn.cursor()
+
 
                     def delete_pre_tweet(politician):
-                        # query = 'delete from PolTweets WHERE politician=?'
-                        # c.execute(query, (politician_name,))
-                        # conn.commit()
-                        # print(politician)
+
 
                         tweetdelete = tweets.query.filter_by(politician=politician).first()
                         db.session.delete(tweetdelete)
                         db.session.commit()
 
-
-
-
-
                     politician_name = politician
                     politicianid = politician_id
 
 
-                    # delete_pre_tweet(politician)
+                    selection = tweets.query.filter_by(politician=politician).first()
 
-                    # print(i)
                     if 'http' in i:
-                        # print(i)
-                        # print('HTTP')
                         i = i[:-24]
-                        # print(i)
 
                     if 'https' in i:
-                        # print('https')
                         num = i.find('https')
-                        # print(num)
                         l = len(i)
-                        # print(l)
-                        # print(num - l)
                         p = num - l
                         i = i[:p]
 
                     if '&amp' in i:
-                        # print(i)
-                        # print(type(i))
                         i = i.replace("&amp", "&")
-                        # print('&')
-                        # print(i)
+
 
 
                     def tweet_entry():
@@ -186,29 +139,28 @@ while True:
                         profile_url = profile
                         print(politician_name)
                         print(tweet)
-                        # print(profile_url)
-                        # c.execute("INSERT  into PolTweets (politician, politician_id, datestamp, tweet) VALUES (?, ?, ?, ?)",
-                        #           (politician, politician_id, datestamp, tweet))
-                        # conn.commit()
-                        # c.close()
-                        # conn.close()
-                        # print(i)
-                        # print(politician, politician_id, datestamp, tweet, background, profile)
                         tweetupdate = tweets(politician=politician, politician_id=politician_id, datestamp=datestamp, tweet=tweet, background_url=background_url, profile_url=profile_url)
-                        # tweetupdate = tweets(politician=politician, politician_id=politician_id, datestamp=datestamp, tweet=tweet)
-
                         db.session.add(tweetupdate)
                         db.session.commit()
 
-                    # print('tweet entry')
-                    # print(i)
-                    tweet_entry()
+                    def delete_pre_tweet(politician):
+                        tweetdelete = tweets.query.filter_by(politician=politician).first()
+                        db.session.delete(tweetdelete)
+                        db.session.commit()
 
-                    # print(politician)
-                    # print(i)
-                    # print(50 * '#')
-                    # c.close()
-                    # conn.close()
+                    politician_name = politician
+                    politicianid = politician_id
+
+                    if selection == None:
+                        print('No pre-tweet')
+                        tweet_entry()
+                        continue
+                    if politician == selection.politician:
+                        print('Updated Tweet')
+                        delete_pre_tweet(politician)
+                        tweet_entry()
+
+
                     break
 
                 except tweepy.TweepError as e:
