f851527600f8ead45f7b0efa8ddd42779aaace0e
Ryan Erickson <ryanerickson74@gmail.com>
2017-11-01 11:50:45 -0500

    &amp str.replace - &

--- config.py
+++ config.py
@@ -4,8 +4,14 @@ import os
 # default config
 class BaseConfig(object):
     DEBUG = False
-    SECRET_KEY = '\xfd{H\xe5<\x95\xf9\xe3\x96.5\xd1\x01O<!\xd5\xa2\xa0\x9fR"\xa1\xa8'
-    SQLALCHEMY_DATABASE_URI = os.environ['sqlite:///political_hangmanPSQL.db']
+    SECRET_KEY = '\xc0\xc3\xe42\xb6\x0cl\x93\xfd\x8e\xfd(\xb7\x8de\x9an\x86\x19\xea\x87\xb5\x1f\xea'
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///political_hangmanPSQL.db'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
+    # consumer_key = "tgOf65xyMVuk98JMIJ6o8OV0w"
+    # consumer_secret = "FUc8m2bpTIDKK6FoSBYj5jpsC5sG7fcFqS5ZGAuMdLLJqkS7CS"
+    # access_token = "2401698925-jgzfZofScmvzyzfUWdxgI9FZuIXmjMrdt3NalJa"
+    # access_token_secret = "U8LWL3VwHg4BTAgWWEDLitjrpoRG2MkKoKWTIXgtMKUDn"
 
 
 class DevelopmentConfig(BaseConfig):
@@ -13,4 +19,4 @@ class DevelopmentConfig(BaseConfig):
 
 
 class ProductionConfig(BaseConfig):
-    DEBUG = Fals
+    DEBUG = False
