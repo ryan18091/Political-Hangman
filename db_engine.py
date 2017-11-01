ac0142766071a72daae2dafe870f9507e13c33e3
Ryan Erickson <ryanerickson74@gmail.com>
2017-11-01 14:30:43 -0500

    removed backgroung and profile url

--- db_engine.py
+++ db_engine.py
@@ -137,9 +137,15 @@ while True:
                         tweet = i
                         background_url = background
                         profile_url = profile
-                        print(politician_name)
+                        print(politician)
+                        print(politician_id)
+                        print(datestamp)
                         print(tweet)
-                        tweetupdate = tweets(politician=politician, politician_id=politician_id, datestamp=datestamp, tweet=tweet, background_url=background_url, profile_url=profile_url)
+                        print(background_url)
+                        print(profile_url)
+                        # tweetupdate = tweets(politician=politician, politician_id=politician_id, datestamp=datestamp, tweet=tweet, background_url=background_url, profile_url=profile_url)
+                        tweetupdate = tweets(politician=politician, politician_id=politician_id, datestamp=datestamp, tweet=tweet)
+
                         db.session.add(tweetupdate)
                         db.session.commit()
 
