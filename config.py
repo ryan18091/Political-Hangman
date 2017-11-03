f0ee13818a900f7c6e272670d7786842e02147db
Ryan Erickson <ryanerickson74@gmail.com>
2017-11-03 18:09:14 -0500

    updated app

--- config.py
+++ config.py
@@ -3,12 +3,12 @@ import os
 
 # default config
 class BaseConfig(object):
-    DEBUG = False
+    DEBUG = True
     SECRET_KEY = '\xc0\xc3\xe42\xb6\x0cl\x93\xfd\x8e\xfd(\xb7\x8de\x9an\x86\x19\xea\x87\xb5\x1f\xea'
     #for local
-    # SQLALCHEMY_DATABASE_URI = 'sqlite:///political_hangmanPSQL.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///political_hangmanPSQL.db'
     #for Heroku
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    # SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 
     # consumer_key = "tgOf65xyMVuk98JMIJ6o8OV0w"
