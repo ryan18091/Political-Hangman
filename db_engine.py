2f55ac3ca6a6100dbf69f129f5c690553d129aa8
Ryan Erickson <ryanerickson74@gmail.com>
2017-11-04 10:55:55 -0500

    updated db engine for heroku background run setup

--- db_engine.py
+++ db_engine.py
@@ -52,144 +52,144 @@ api = tweepy.API(auth)
 # c.close()
 # conn.close()
 
-while True:
+# while True:
 
     # for politician in pol_list:
-    from pol_ids import t_dict
+from pol_ids import t_dict
 
-    for politician, politician_id in t_dict.items():
+for politician, politician_id in t_dict.items():
 
-        time.sleep(6)
-        print("Twitter API Engine Running...")
-        for tweet in tweepy.Cursor(api.user_timeline, id=politician_id, tweet_mode='extended').items(10):
+    time.sleep(6)
+    print("Twitter API Engine Running...")
+    for tweet in tweepy.Cursor(api.user_timeline, id=politician_id, tweet_mode='extended').items(10):
 
 
-            if (not tweet.retweeted) and ('RT @' not in tweet.full_text):
-                try:
-                    t = (tweet)
-                    # print(t)
-                    i = (tweet.full_text)
-                    # created_at = (tweet.created_at)
-                    # print(created_at)
-                    # screen_name = (tweet.screen_name)
-                    # print(screen_name)
+        if (not tweet.retweeted) and ('RT @' not in tweet.full_text):
+            try:
+                t = (tweet)
+                # print(t)
+                i = (tweet.full_text)
+                # created_at = (tweet.created_at)
+                # print(created_at)
+                # screen_name = (tweet.screen_name)
+                # print(screen_name)
+                mystring = str(t)
+
+                screen_name = (tweet.user)
+                screen_name = (screen_name.screen_name)
+                print(screen_name)
+
+
+
+                def find_between1(first, last):
                     mystring = str(t)
-
-                    screen_name = (tweet.user)
-                    screen_name = (screen_name.screen_name)
-                    # print(screen_name)
-
-
-
-                    def find_between1(first, last):
-                        mystring = str(t)
-                        try:
-                            start = mystring.index(first) + len(first)
-                            end = mystring.index(last, start)
-                            background_url= mystring[start:end]
-                            background_url = background_url[4:]
-                            background = background_url[:-4]
-                            return background
-                        except ValueError:
-                            return ""
+                    try:
+                        start = mystring.index(first) + len(first)
+                        end = mystring.index(last, start)
+                        background_url= mystring[start:end]
+                        background_url = background_url[4:]
+                        background = background_url[:-4]
+                        return background
+                    except ValueError:
+                        return ""
 
 
-                    background = (find_between1('profile_banner_url' , 'profile_link_color'))
-                    # print(background)
+                background = (find_between1('profile_banner_url' , 'profile_link_color'))
+                # print(background)
 
-                    def find_between2(first, last):
-                        try:
-                            start = mystring.index(first) + len(first)
-                            end = mystring.index(last, start)
-                            profile_url = mystring[start:end]
-                            profile_url = profile_url[4:]
-                            profile = profile_url[:-4]
-                            return profile
-                        except ValueError:
-                            return ""
+                def find_between2(first, last):
+                    try:
+                        start = mystring.index(first) + len(first)
+                        end = mystring.index(last, start)
+                        profile_url = mystring[start:end]
+                        profile_url = profile_url[4:]
+                        profile = profile_url[:-4]
+                        return profile
+                    except ValueError:
+                        return ""
 
 
-                    profile = (find_between2('profile_image_url', 'profile_image_url_https'))
-                    # print(profile)
+                profile = (find_between2('profile_image_url', 'profile_image_url_https'))
+                # print(profile)
 
 
 
 
 
-                    def delete_pre_tweet(politician):
+                def delete_pre_tweet(politician):
 
 
-                        tweetdelete = tweets.query.filter_by(politician=politician).first()
-                        db.session.delete(tweetdelete)
-                        db.session.commit()
+                    tweetdelete = tweets.query.filter_by(politician=politician).first()
+                    db.session.delete(tweetdelete)
+                    db.session.commit()
 
-                    politician_name = politician
-                    politicianid = politician_id
-
-
-                    selection = tweets.query.filter_by(politician=politician).first()
-
-                    if 'http' in i:
-                        i = i[:-24]
-
-                    if 'https' in i:
-                        num = i.find('https')
-                        l = len(i)
-                        p = num - l
-                        i = i[:p]
-
-                    if '&amp' in i:
-                        i = i.replace("&amp", "&")
+                politician_name = politician
+                politicianid = politician_id
 
 
+                selection = tweets.query.filter_by(politician=politician).first()
 
-                    def tweet_entry():
-                        politician = politician_name
-                        # politician_id = politicianid
-                        datestamp = str(datetime.now())
-                        tweet = i
-                        # background_url = background
-                        # profile_url = profile
-                        # print(politician)
-                        # # print(politician_id)
-                        # # print(datestamp)
-                        # # print(tweet)
-                        # # print(background_url)
-                        # # print(profile_url)
-                        # tweetupdate = tweets(politician=politician, politician_id=politician_id, datestamp=datestamp, tweet=tweet, background_url=background_url, profile_url=profile_url)
-                        tweetupdate = tweets(politician=politician, datestamp=datestamp, tweet=tweet, screen_name=screen_name)
+                if 'http' in i:
+                    i = i[:-24]
 
-                        db.session.add(tweetupdate)
-                        db.session.commit()
+                if 'https' in i:
+                    num = i.find('https')
+                    l = len(i)
+                    p = num - l
+                    i = i[:p]
 
-                    def delete_pre_tweet(politician):
-                        tweetdelete = tweets.query.filter_by(politician=politician).first()
-                        db.session.delete(tweetdelete)
-                        db.session.commit()
-
-                    politician_name = politician
-                    politicianid = politician_id
-
-                    if selection == None:
-                        # print('No pre-tweet')
-                        tweet_entry()
-                        continue
-                    if politician == selection.politician:
-                        # print('Updated Tweet')
-                        delete_pre_tweet(politician)
-                        tweet_entry()
+                if '&amp' in i:
+                    i = i.replace("&amp", "&")
 
 
-                    break
 
-                except tweepy.TweepError as e:
-                    print('Tweepy Error')
-                    print(e.reason)
-                    sleep(10)
+                def tweet_entry():
+                    politician = politician_name
+                    # politician_id = politicianid
+                    datestamp = str(datetime.now())
+                    tweet = i
+                    # background_url = background
+                    # profile_url = profile
+                    # print(politician)
+                    # # print(politician_id)
+                    # # print(datestamp)
+                    # # print(tweet)
+                    # # print(background_url)
+                    # # print(profile_url)
+                    # tweetupdate = tweets(politician=politician, politician_id=politician_id, datestamp=datestamp, tweet=tweet, background_url=background_url, profile_url=profile_url)
+                    tweetupdate = tweets(politician=politician, datestamp=datestamp, tweet=tweet, screen_name=screen_name)
+
+                    db.session.add(tweetupdate)
+                    db.session.commit()
+
+                def delete_pre_tweet(politician):
+                    tweetdelete = tweets.query.filter_by(politician=politician).first()
+                    db.session.delete(tweetdelete)
+                    db.session.commit()
+
+                politician_name = politician
+                politicianid = politician_id
+
+                if selection == None:
+                    # print('No pre-tweet')
+                    tweet_entry()
                     continue
-                except StopIteration:
-                    break
+                if politician == selection.politician:
+                    # print('Updated Tweet')
+                    delete_pre_tweet(politician)
+                    tweet_entry()
 
-            if (tweet.retweeted) and ('RT @' in tweet.text):
-                # print('Retweet')
+
+                break
+
+            except tweepy.TweepError as e:
+                print('Tweepy Error')
+                print(e.reason)
+                sleep(10)
                 continue
+            except StopIteration:
+                break
+
+        if (tweet.retweeted) and ('RT @' in tweet.text):
+            # print('Retweet')
+            continue
