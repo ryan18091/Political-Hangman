fd309ed2398318e9ea80dab8f473b0444bedf7ad
Ryan Erickson <ryanerickson74@gmail.com>
2017-11-05 09:56:20 -0600

    updated app config files

--- pol_ids.py
+++ pol_ids.py
@@ -7,7 +7,18 @@ t_dict = {'Donald Trump': 25073877, 'Bernie Sanders': 216776631,'Mike Pence': 22
           'Luther Strange':829794295355940866, 'Lisa Murkowski':18061669,'John McCain':  19394188, 'Jeff Flake':16056306,
           'John Boozman':5558312, 'Bradley Byrne':2253968388, 'Martha Roby':224294785, 'Mike Rogers':33977070,
           'Robery Aderholt':76452765, 'Mo Brooks':237299871, 'Gary Palmer': 2861616083, 'Terri A. Sewell':381152398,
-          'Don Young': 37007274,}
+          'Don Young': 37007274,'Mike Pompeo':817404026992152579, 'Scott Pruitt':832672006113931269, 'Tom Cotton':968650362,
+          'Dienne Feinstein':476256944,'Kamala Harris':803694179079458816,'Michael Bennet':224285242,
+          'Cory Gardner':235217558,'Richard Blumenthal':278124059,'Chris Murphy':150078976,'Tom Carper':249787913,
+          'Chris Coons':15324851,'Bill Nelson':20597460,'Marco Rubio':15745368,'Johnny Isakson':253252536,
+          'David Perdue':2863210809,'Brain Schatz':1262099252,'Mazie Hirono':92186819,'Mike Crapo':600463589,
+          'Jim Risch':1096059529,'Dick Durbin':308794407,'Tammy Duckworth':1058520120,'Joe Donnelly':216503958,
+          'Todd Young':59969802,'Chuck Grassley':10615232,'Joni Ernst':1383059977,'Pat Roberts':75364211,
+          'Jerry Moran':18632666,'Mitch McConnell':339822881,'Rand Paul':39834947,'Bill Cassidy':55677432,
+          'John N. Kennedy':816683274076614656,'Susan Collins':19726613,'Angus King':1068481578,
+          'Ben Cardin':109071031,'Chris Van Hollen':18137749,'Elizabeth Warren':70207298,'Ed Markey':21406834,
+          'Debbie Stabenow':76456274,'Gary Peters':236511574,'Amy Klobuchar':33537967,'Al Franken':171968009,
+          'Thad Cochran':555474658,'Roger Wicker':264219447,'Claire McCaskill':57065141}
 
 
 
