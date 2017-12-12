5f29a6516d1f9cd35571586e5b6119289d24062f
Ryan Erickson <ryanerickson74@gmail.com>
2017-12-12 15:11:52 -0600

    updated app config files

--- app.py
+++ app.py
@@ -37,9 +37,6 @@ turns = ()
 # app = Flask(__name__)
 # app.secret_key = '\xfd{H\xe5<\x95\xf9\xe3\x96.5\xd1\x01O<!\xd5\xa2\xa0\x9fR"\xa1\xa8'
 
-from PH_Parser import *
-print('running email')
-runemail()
 
 def get_phrase(session_id):
 
