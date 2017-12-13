7a60edcc84de7265400f1affd6908d74aa5917d5
Ryan Erickson <ryanerickson74@gmail.com>
2017-12-13 10:50:31 -0600

    updated app config files

--- pol_ids.py
+++ pol_ids.py
@@ -1,4 +1,4 @@
-t_dict = {'Donald Trump': 25073877, 'Bernie Sanders': 216776631,'Mike Pence': 22203756, 'Rex Tillerson':
+t_dict = {'Donald Trump': 250738771, 'Bernie Sanders': 216776631,'Mike Pence': 22203756, 'Rex Tillerson':
     9624742, 'Richard Shelby': 21111098,'Dan Sullivan': 2891210047, 'Steven T. Mnuchin':799764016885329921, 'Jeff Sessions':986781648,
           'Ryan Zinke': 1906282568, 'Sonny Perdue':842072478834909184, 'Wilbur L. Ross, Jr.': 836305915452272641,
           'Alexander Acosta': 819980276570976256, 'Benjamin S. Carson, Sr.':828613457020870657, 'Elaine L. Chao': 826065858548133888,
